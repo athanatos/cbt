d41b61fc94983e7c7511050d44b07a8b0469e505
Samuel Just <sjust@redhat.com>
2021-11-29 14:51:38 -0800

    tools/crimson/seastore_fio_run.sh: avoid cd
    Allows relative paths to work as expected.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- tools/crimson/seastore_fio_run.sh
+++ tools/crimson/seastore_fio_run.sh
@@ -5,6 +5,7 @@ TOP_DIR=$(cd $(dirname "$0") && pwd)
 # configurations
 RESULT_DIR=${RESULT_DIR:-"$TOP_DIR/results"}
 BUILD_DIR=${BUILD_DIR:-"~/ceph/build/"}
+export CEPH_CONF=${CEPH_CONF:-"$BUILD_DIR/ceph.conf"}
 FIO_CONF=${FIO_CONF:-"~/ceph/rbd_write.fio"}
 POOL_NAME="rbd"
 POOL_NUM=128
@@ -19,21 +20,20 @@ if [ -e $RESULT_DIR ]; then
 fi
 
 mkdir -p $RESULT_DIR
-cd $BUILD_DIR
 CURRENT_ROUND=0
 TARGET_ROUND=$(( CURRENT_ROUND + TOTAL_ROUND ))
 
-CEPH_DEV=1 ./bin/ceph osd pool create $POOL_NAME $POOL_NUM $POOL_NUM
-CEPH_DEV=1 ./bin/ceph osd pool set --yes-i-really-mean-it $POOL_NAME size 1 && ./bin/ceph osd pool --yes-i-really-mean-it set $POOL_NAME  min_size 1
-CEPH_DEV=1 ./bin/rbd create $IMAG_NAME --size 2G --image-format=2 --rbd_default_features=3
+CEPH_DEV=1 $BUILD_DIR/bin/ceph osd pool create $POOL_NAME $POOL_NUM $POOL_NUM
+CEPH_DEV=1 $BUILD_DIR/bin/ceph osd pool set --yes-i-really-mean-it $POOL_NAME size 1 && $BUILD_DIR/bin/ceph osd pool --yes-i-really-mean-it set $POOL_NAME  min_size 1
+CEPH_DEV=1 $BUILD_DIR/bin/rbd create $IMAG_NAME --size 2G --image-format=2 --rbd_default_features=3
 
-CEPH_DEV=1 ./bin/ceph tell osd.0 dump_metrics 2>&1 | tee $RESULT_DIR/result_${CURRENT_ROUND}_metrics.log
+CEPH_DEV=1 $BUILD_DIR/bin/ceph tell osd.0 dump_metrics 2>&1 | tee $RESULT_DIR/result_${CURRENT_ROUND}_metrics.log
 while [ $CURRENT_ROUND -lt $TARGET_ROUND ]
 do
   (( ++CURRENT_ROUND ))
   echo "start round $CURRENT_ROUND ..."
   CEPH_DEV=1 fio $FIO_CONF --output=$RESULT_DIR/result_${CURRENT_ROUND}_bench.log
-  CEPH_DEV=1 ./bin/ceph tell osd.0 dump_metrics 2>&1 | tee $RESULT_DIR/result_${CURRENT_ROUND}_metrics.log
+  CEPH_DEV=1 $BUILD_DIR/bin/ceph tell osd.0 dump_metrics 2>&1 | tee $RESULT_DIR/result_${CURRENT_ROUND}_metrics.log
   echo "finish round $CURRENT_ROUND"
   echo
   sleep 2
