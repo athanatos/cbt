913ec48556349aab4cf8efa0dfcbc7a0efe336df
Samuel Just <sjust@redhat.com>
2024-01-05 16:03:06 -0800

    more dependency fixing

--- setup.sh
+++ setup.sh
@@ -7,18 +7,7 @@ sudo yum check-update
 sudo yum -y update
 sudo yum install -y psmisc util-linux coreutils xfsprogs e2fsprogs findutils \
   git wget bzip2 make automake gcc gcc-c++ kernel-devel perf blktrace lsof \
-  sysstat screen python3-yaml ipmitool dstat zlib-devel
-
-MIRROR="http://mirror.hmc.edu/fedora/linux/releases/22/Everything/x86_64/os/Packages"
-
-wget ${MIRROR}/p/pdsh-2.31-3.fc22.x86_64.rpm
-wget ${MIRROR}/p/pdsh-2.31-3.fc22.x86_64.rpm
-wget ${MIRROR}/p/pdsh-rcmd-ssh-2.31-3.fc22.x86_64.rpm
-wget ${MIRROR}/c/collectl-4.0.0-1.fc22.noarch.rpm
-wget ${MIRROR}/i/iftop-1.0-0.9.pre4.fc22.x86_64.rpm
-wget ${MIRROR}/i/iperf3-3.0.10-1.fc22.x86_64.rpm
-
-sudo yum localinstall -y *.rpm
+  sysstat screen python3-yaml ipmitool dstat zlib-devel pdsh iftop iperf3
 
 git clone https://github.com/axboe/fio.git
 git clone https://github.com/andikleen/pmu-tools.git
