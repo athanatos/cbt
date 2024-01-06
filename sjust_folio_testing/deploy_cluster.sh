0f90357bc251c710a877343d6e62e4047e33c4c4
Samuel Just <sjust@redhat.com>
2024-01-05 17:05:36 -0800

    setting up cephadm deployment script

---
+++ sjust_folio_testing/deploy_cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CEPH_SHA1=9d465d1f4bbf3adc8c5d0a2ed062f92b7dccc555
+
+# download cephadm
+
+CEPHADM_RELEASE=18.2.1
+BINDIR=/home/sjust/bin
+cd ${BINDIR}
+curl --silent --remote-name --location https://download.ceph.com/rpm-${CEPHADM_RELEASE}/el9/noarch/cephadm
+chmod +x cephadm
+
+# bootstrap cluster
+
+./cephadm boostrap \
+  --image quay.ceph.io/ceph-ci/ceph:${CEPH_SHA1}-crimson \
+	--allow-mismatched-release bootstrap \
+	--mon-ip 127.0.0.1 \
+	--log-to-file
