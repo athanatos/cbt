43856ac002ac5054e093cb31f88e6883039ac644
Samuel Just <sjust@redhat.com>
2024-01-11 15:14:57 -0800

    experimenting with crimson_auto_bench.py

--- sjust_folio_testing/run_autobench.sh
+++ sjust_folio_testing/run_autobench.sh
@@ -7,4 +7,4 @@ BENCH_CONF=${SJUST_DIR}/bench_config.yaml
 
 OUTPUT_DIR=~/output/
 
-${CBT_DIR}/tools/crimson/crimson_auto_bench.py --run --x crimson --config ${BENCH_CONF}
+${CBT_DIR}/tools/crimson/crimson_auto_bench.py --run --x clients --config ${BENCH_CONF}
