eea1236fcdc4cabb9547b257dbb00dac93309b31
Samuel Just <sjust@redhat.com>
2024-01-18 15:17:21 -0800

    working

--- sjust_folio_testing/run_autobench.sh
+++ sjust_folio_testing/run_autobench.sh
@@ -14,5 +14,5 @@ git reset --hard origin/sjust/integration/wip-crimson-testing
 cd ${CEPH_DIR}/build
 
 ninja -j50 all tests
-${CAB_DIR}/crimson_auto_bench.py --run --x client --config ${BENCH_CONF}
+${CAB_DIR}/crimson_auto_bench.py --run --x smp --config ${BENCH_CONF}
 #${CAB_DIR}/crimson_bench_tool.py --client 4 --thread 2 --bench-taskset 16-31 --time 60 --block-size 4096 --dev /dev/nvme0n1 --fio-rbd-rand-write 1
