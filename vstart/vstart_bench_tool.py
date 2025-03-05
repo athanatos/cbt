d168f7fefd88281243c2869eeea8e02b2b8d5991
Samuel Just <sjust@redhat.com>
2025-03-04 19:52:44 -0800

    support arbitrary vstart options

--- vstart/vstart_bench_tool.py
+++ vstart/vstart_bench_tool.py
@@ -200,7 +200,7 @@ class VStartCluster(Cluster):
                 ret += ['--seastore']
                 if self.seastore_devices is not []:
                     ret += ['--seastore-devs', ','.join(self.seastore_devices)]
-        osd_options=' '.join([f'--{k}={v}' for k, v in self.osd_options.items()))
+        osd_options=' '.join([f'--{k}={v}' for k, v in self.osd_options.items()])
         if osd_options is not '':
             ret += f"""--osd-args='{osd_options}'"""
         return ret
