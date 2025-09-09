c1f6f33c5654ab57912c0b09a8857963fdcf8404
Samuel Just <sjust@redhat.com>
2025-09-09 15:51:55 +0000

    min_flush

--- vstart/vstart_bench_tool.py
+++ vstart/vstart_bench_tool.py
@@ -763,8 +763,16 @@ class Counters(PerfMonitor):
                 ('nbd_average_io_depth', safe_div,
                  'seastore_nbd_write_io_depth_total',
                  'seastore_nbd_write_count'),
+                ('nbd_write_rate', safe_div,
+                 'seastore_nbd_write_count',
+                 'seastore_nbd_record_time'),
+                ('nbd_idle_ratio', safe_div,
+                 'seastore_nbd_idle_time',
+                 'seastore_nbd_record_time'),
+                ('nbd_busy_ratio', safe_div,
+                 'seastore_nbd_busy_time',
+                 'seastore_nbd_record_time'),
                 'seastore_nbd_write_count'
-                'seastore_nbd_nvme_write_count'
             ]
             self.collapse = set([
                 'shard',
